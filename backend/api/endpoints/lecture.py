ba5185379330740cf271c294c39f1bc2b680fe34
admin <nimish.lingesh@gmail.com>
2024-05-05 11:06:51 -0700

    cleanup

--- backend/api/endpoints/lecture.py
+++ backend/api/endpoints/lecture.py
@@ -11,64 +11,12 @@ log = logging.getLogger(__name__)
 
 router = APIRouter()
 
-# @router.get("/lectures")
-# def get_lectures(
-#     courseId: str = None,
-#     db: Session = Depends(get_db)
-# ):  
-#     """
-#     API endpoint to get all lectures.
-#     Params:
-#     """
-#     query: Query = db.query(LectureModel)
-#     if courseId is None:
-#         return {"error": "pass courseId as the param"}
-#     query = query.filter(LectureModel.courseId == courseId) 
-#     lectures = query.all()
-#     return lectures
-
-# @router.post("/uploadLecture")
-# def upload_lectures(
-#     response: Response,
-#     body: lecture.UploadLecture,
-#     upload_file: UploadFile = File(...),
-#     db: Session = Depends(get_db)
-# ):
-#     """
-#     Upload the lecture into S3 (video/pdf)
-#     Stores its metadata in postgres lecture table
-#     """
-#     try:
-#         exists_lec = db.query(LectureModel).filter(LectureModel.id == body.id)
-#         if exists_lec:
-#             return f"Failed: Lecture with Id {body.id} already exists"
-        
-#         l = body.dict()
-#         new_lecture = LectureModel(**l)
-        
-#         # TODO - check for the multiple upload_file (.pdf/.mp3) and save it in videoURL/pdfURL column
-#         # Upload the files to the S3 and get the URL
-#         url_link = utils.uploadFileToS3(upload_file.filename, 'intellitool-bucket', upload_file)
-
-#         # TODO
-#         # store the URL in videoURL/pdfURL column
-#         new_lecture.videoURL = None
-#         new_lecture.pdfURL = None
-
-#         db.add(new_lecture)
-#         db.commit()
-#         db.refresh(new_lecture)
-#     except Exception as e:
-#         return f"Error: {e}"
-#     return f"Success: Added Lecture {body.id}"
-
-
 @router.post("/uploadLecture/")
 async def upload_lectures(
     id: int = Form(...),
     name: str = Form(...),
     date: str = Form(...),
-    courseId: str = Form(...),
+    courseId: int = Form(...),
     courseName: str = Form(...),
     video: UploadFile = File(None),
     pdf: UploadFile = File(None),
@@ -100,9 +48,9 @@ async def upload_lectures(
         date=date,
         courseId=courseId,
         courseName=courseName,
-        video_url=video_url,
-        pdf_url=pdf_url,
-        image_url=image_url,
+        videoURL=video_url,
+        pdfURL=pdf_url,
+        imageURL=image_url,
         content=contents
     )
     db.add(new_lecture)
