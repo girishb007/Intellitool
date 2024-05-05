e111ccd6fe4f1da92f7a50e50d4731dc57ce4de7
Devansh Bansal <devanshd0@gmail.com>
2024-05-04 18:23:21 -0700

    refactoring

--- backend/api/api.py
+++ backend/api/api.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Response, Request, Query
 
 from pydantic import BaseModel
 from typing import List, Optional
-from api.endpoints import user, student, professor, admin, course
+from api.endpoints import user, professor, admin, course, summary, student
 
 router = APIRouter()
 
@@ -12,4 +12,5 @@ router.include_router(student.router)
 router.include_router(professor.router)
 router.include_router(admin.router)
 router.include_router(course.router)
+router.include_router(summary.router)
 
---
+++ backend/api/endpoints/summary.py
@@ -0,0 +1,76 @@
+import logging
+from fastapi import APIRouter, File, Response, Depends, HTTPException, UploadFile
+from schemas import user
+from sqlalchemy.orm import Session, Query
+from sqlalchemy.exc import IntegrityError
+from db.database import get_db
+from models import UserModel
+import os
+import boto3
+from openai import OpenAI
+from moviepy import editor as mp
+
+
+OpenAI.api_key = 'sk-proj-Q3Em8tl42Ds1GkUPFI3nT3BlbkFJjeoEbGc0fTlSCTRp5KXk'
+os.environ['OPENAI_API_KEY'] = 'sk-proj-Q3Em8tl42Ds1GkUPFI3nT3BlbkFJjeoEbGc0fTlSCTRp5KXk'
+client = OpenAI()
+
+router = APIRouter()
+
+@router.get("/getSummaries")
+def testConn():
+    #return "Working"
+
+    audio_file= open("/Users/spartan/Desktop/audiotestfile.mp3", "rb")
+    transcript = client.audio.transcriptions.create(
+    model="whisper-1", 
+    file=audio_file
+    )
+
+    return transcript.text
+
+@router.get("/videoToAudioConverter")
+def convertVideo():
+    # Insert Local Video File Path 
+    clip = mp.VideoFileClip(r"/Users/spartan/Desktop/testvideofile.mov")
+    
+    # Insert Local Audio File Path
+    clip.audio.write_audiofile(r"/Users/spartan/Desktop/convertedvideofile.mp3")
+
+@router.get("/downloadfile")
+def getFileFromS3():
+    s3 = boto3.client('s3', aws_access_key_id="AKIA3FO4UZ66TYQMK6NB" , aws_secret_access_key="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")
+    s3.download_file('intellitool-bucket', 'audiotestfile.mp3', 'download') 
+ 
+
+def uploadFileToS3(file_name, bucket, object_name=None):
+    if object_name is None:
+        object_name = os.path.basename(file_name)
+
+    s3 = boto3.client('s3', aws_access_key_id="AKIA3FO4UZ66TYQMK6NB" , aws_secret_access_key="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")
+    try:
+        response = s3.upload_file(file_name, bucket, object_name)
+    except Exception as e:
+        logging.error(e)
+        return False
+    return True
+
+@router.post("/uploadFile")
+async def upload_image(upload_file:UploadFile =File(...)):
+
+    if '.jpg' in upload_file.filename or '.jpeg' in upload_file.filename or '.png' in upload_file.filename or '.pdf' in upload_file.filename or '.mp3' in upload_file.filename or '.mp4' in upload_file.filename:
+        file_save_path="./uploads/"+upload_file.filename
+        if os.path.exists("./uploads") == False:
+            os.makedirs("./uploads")
+
+        with open(file_save_path, "wb") as f:
+            f.write(upload_file.file.read())
+
+        uploadFileToS3(file_name=file_save_path, bucket='intellitool-bucket')
+
+        if os.path.exists(file_save_path):
+            return {"File save path":file_save_path,"message": "File saved successfully"}
+        else:
+            return {"error":"File Not saved !!!"}
+    else:
+        return {"error": "File Type is not valid please upload only jpg,jpeg, png, pdf, mp3 and .mp4 only"}
