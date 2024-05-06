755ec5eb37fd358c84fe0a70d6b37de05f36d8d5
Devansh Bansal <devanshd0@gmail.com>
2024-05-05 19:08:26 -0700

    utils cleanup

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -5,29 +5,10 @@ from fastapi import HTTPException
 from openai import OpenAI
 from moviepy import editor as mp
 
-# def uploadFileToS3(file_name, bucket, object_name=None):
-#     if object_name is None:
-#         object_name = os.path.basename(file_name)
 
-#     s3 = boto3.client('s3', aws_access_key_id="AKIA3FO4UZ66TYQMK6NB" , aws_secret_access_key="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")
-#     try:
-#         response = s3.upload_file(file_name, bucket, object_name)
-#     except Exception as e:
-#         logging.error(e)
-#         return False
-#     return True
 s3_client = boto3.client('s3', aws_access_key_id="AKIA3FO4UZ66TYQMK6NB" , aws_secret_access_key="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")
 
 
-def uploadFileToS3(file, bucket_name, object_name=None):
-    """Upload a file to an S3 bucket"""
-    if object_name is None:
-        object_name = file.filename
-    try:
-        resp = s3_client.upload_fileobj(file.file, bucket_name, object_name)
-        return f"https://{bucket_name}.s3.amazonaws.com/{object_name}"
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
     
 def convertVideoToMp3(file_name):
     try:
@@ -36,17 +17,6 @@ def convertVideoToMp3(file_name):
     except Exception as e:
         print(f"Caught exception while converting video tp Mp3: {e}")
 
-
-def extractTextFromVideo(video):
-    try:
-        audio_file = open("converted_video.mp3", "rb")
-        transcript = client.audio.transcriptions.create(
-        model="whisper-1", 
-        file=audio_file
-        )
-        print(transcript.text)   
-    except Exception as e:
-        print(f"Caught exception while extracting text from video: {e}")
 
 def getTextFromImage(fileName):
     print("Getting text from Image")
