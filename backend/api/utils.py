63d4877d6b151c447e6715e9e867cebbdaaf697c
Devansh Bansal <devanshd0@gmail.com>
2024-05-05 19:10:50 -0700

    Adding a utility function to upload file to S3

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -9,7 +9,20 @@ from moviepy import editor as mp
 s3_client = boto3.client('s3', aws_access_key_id="AKIA3FO4UZ66TYQMK6NB" , aws_secret_access_key="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")
 
 
-    
+def uploadFileToS3(file_name, bucket, object_name=None):
+    if object_name is None:
+        object_name = os.path.basename(file_name)
+
+    noException = True
+
+    s3 = boto3.client('s3', aws_access_key_id="AKIA3FO4UZ66TYQMK6NB" , aws_secret_access_key="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")
+    try:
+        response = s3.upload_file(file_name, bucket, object_name)
+        print(f"Got this repsonse from S3: {response} ")
+    except Exception as e:
+        print(f"Caught exception while uploading file to S3: {e}" )
+    return noException
+   
 def convertVideoToMp3(file_name):
     try:
         clip = mp.VideoFileClip(file_name)
